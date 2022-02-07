6c27545d93b9d90e027f4232be4c73cae6812040
herveev <herveevoda@gmail.com>
2022-02-07 15:42:39 +0100

    First commit

---
+++ helloScript.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+
+
+#Hello Bash script
+
+echo "Hello bash linux hint audience" 
+
+#Redirect output to file
+
+echo "Write this string to file.txt" > file.txt
+
+
+#Write to a file from the shell
+
+cat > file1.txt
+
+#Write to a file from the shell and Append text at the end of the current file 
+
+cat > file2.txt
+cat >> file2.txt
+
+
